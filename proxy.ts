f6118acb75b1620284cb41595275c2ff711f9472
Octavio Chicco <octaviotrabajo2026@gmail.com>
2026-03-22 11:01:02 -0300

    test proxy simple

--- proxy.ts
+++ proxy.ts
@@ -1,27 +1,9 @@
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function proxy(request: NextRequest) {
-  let response = NextResponse.next();
-
-  const url = request.nextUrl;
-  const host = request.headers.get("host")?.toLowerCase() || "";
-
-  const rootDomain = process.env.NEXT_PUBLIC_ROOT_DOMAIN?.replace("https://", "").replace("http://", "").trim().toLowerCase();
-
-  if (url.pathname.startsWith("/_next") || url.pathname.startsWith("/api") || url.pathname.startsWith("/static")) {
-    return response;
-  }
-
-  if (host === rootDomain || host.includes("vercel.app") || host.startsWith("localhost")) {
-    return response;
-  }
-
-  const searchParams = url.searchParams.toString();
-  const path = `${url.pathname}${searchParams.length > 0 ? `?${searchParams}` : ""}`;
-
-  return NextResponse.rewrite(new URL(`/${host}${path}`, request.url));
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|auth/confirm|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)" ],
 };
